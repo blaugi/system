e7a4598ed7ee998c583035fc2630f2b290f6e561
Blaugi <matheusmgirardi@gmail.com>
2026-07-28 14:58:01 -0300

    sync

--- flake.nix
+++ flake.nix
@@ -10,49 +10,6 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-<<<<<<< HEAD
-  outputs = { self, nixpkgs, nixpkgs-fish, home-manager, stylix, ... }@inputs:
-    let
-      system = "x86_64-linux"; 
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      pkgsFish = nixpkgs-fish.legacyPackages.${system};
-
-      username = "blau";
-      homeDirectory = "/home/${username}";
-    in {
-      homeConfigurations = {
-        headless = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          
-          extraSpecialArgs = { 
-            inherit inputs pkgsFish; 
-            username = "azureuser";
-            homeDirectory = "/home/azureuser";
-            isHeadless = true;
-          };
-          
-          modules = [
-            stylix.homeModules.stylix
-            ./home/default.nix
-          ];
-        };
-
-        desktop = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          
-          extraSpecialArgs = { 
-            inherit username homeDirectory inputs pkgsFish; 
-            isHeadless = false;
-          };
-          
-          modules = [
-            stylix.homeModules.stylix
-            ./home/default.nix
-          ];
-        };
-=======
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
@@ -60,7 +17,6 @@
         (inputs.import-tree ./modules)
       ];
       systems = [ "x86_64-linux" ]
->>>>>>> 73f0a73 (sync progress)
       };
     };
 }
